76bc5bb7eb9cec6e22a20eb7f158f52da3895b1e
Martinmessner <martinmessner95@gmail.com>
2023-11-18 09:41:35 -0300

    modify css a module assets

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
-import './condicionales.css';
-import './loading.css';
-import './Eyes.css';
-import './queries.css';
+import '../src/assets/index.css';
+import '../src/assets/condicionales.css';
+import '../src/assets/loading.css';
+import '../src/assets/Eyes.css';
+import '../src/assets/queries.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
